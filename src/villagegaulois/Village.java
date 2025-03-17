c4a658ac9a589d6f54be727f1e61d59ced21a7d0
Hichem212 <hichem.sanhaji@univ-tlse3.fr>
2025-03-17 23:21:24 +0100

    Fin de la première partie

--- src/villagegaulois/Village.java
+++ src/villagegaulois/Village.java
@@ -2,58 +2,139 @@ package villagegaulois;
 
 import personnages.Chef;
 import personnages.Gaulois;
+import personnages.Druide;
 
 public class Village {
-	private String nom;
-	private Chef chef;
-	private Gaulois[] villageois;
-	private int nbVillageois = 0;
+    private String nom;
+    private Chef chef;
+    private Gaulois[] villageois;
+    private int nbVillageois = 0;
+    private Marche marche;
 
-	public Village(String nom, int nbVillageoisMaximum) {
-		this.nom = nom;
-		villageois = new Gaulois[nbVillageoisMaximum];
-	}
+    public Village(String nom, int nbVillageoisMaximum, int nbEtals) {
+        this.nom = nom;
+        villageois = new Gaulois[nbVillageoisMaximum];
+        marche = new Marche(nbEtals);
+    }
 
-	public String getNom() {
-		return nom;
-	}
+    private static class Marche {
+        Etal[] etals;
 
-	public void setChef(Chef chef) {
-		this.chef = chef;
-	}
+        private Marche(int nbEtals) {
+            etals = new Etal[nbEtals];
+            for (int i = 0; i < nbEtals; i++) {
+                etals[i] = new Etal();
+            }
+        }
 
-	public void ajouterHabitant(Gaulois gaulois) {
-		if (nbVillageois < villageois.length) {
-			villageois[nbVillageois] = gaulois;
-			nbVillageois++;
-		}
-	}
+        private void utiliserEtal(int indiceEtal, Gaulois vendeur, String produit, int nbProduit) {
+            if (indiceEtal >= 0 && indiceEtal < etals.length) {
+                etals[indiceEtal].occuperEtal(vendeur, produit, nbProduit);
+            }
+        }
 
-	public Gaulois trouverHabitant(String nomGaulois) {
-		if (nomGaulois.equals(chef.getNom())) {
-			return chef;
-		}
-		for (int i = 0; i < nbVillageois; i++) {
-			Gaulois gaulois = villageois[i];
-			if (gaulois.getNom().equals(nomGaulois)) {
-				return gaulois;
-			}
-		}
-		return null;
-	}
+        private int trouverEtalLibre() {
+            int indiceEtalLibre = -1;
+            for (int i = 0; i < etals.length && indiceEtalLibre < 0; i++) {
+                if (!etals[i].isEtalOccupe()) {
+                    indiceEtalLibre = i;
+                }
+            }
+            return indiceEtalLibre;
+        }
 
-	public String afficherVillageois() {
-		StringBuilder chaine = new StringBuilder();
-		if (nbVillageois < 1) {
-			chaine.append("Il n'y a encore aucun habitant au village du chef "
-					+ chef.getNom() + ".\n");
-		} else {
-			chaine.append("Au village du chef " + chef.getNom()
-					+ " vivent les légendaires gaulois :\n");
-			for (int i = 0; i < nbVillageois; i++) {
-				chaine.append("- " + villageois[i].getNom() + "\n");
-			}
-		}
-		return chaine.toString();
-	}
-}
+        private Etal[] trouverEtals(String produit) {
+            int nbEtal = 0;
+            for (Etal etal : etals) {
+                if (etal.isEtalOccupe() && etal.contientProduit(produit)) {
+                    nbEtal++;
+                }
+            }
+            Etal[] etalsProduitsRecherche = null;
+            if (nbEtal > 0) {
+                etalsProduitsRecherche = new Etal[nbEtal];
+                int nbEtalTrouve = 0;
+                for (int i = 0; i < etals.length && nbEtalTrouve < nbEtal; i++) {
+                    if (etals[i].isEtalOccupe() && etals[i].contientProduit(produit)) {
+                        etalsProduitsRecherche[nbEtalTrouve] = etals[i];
+                        nbEtalTrouve++;
+                    }
+                }
+            }
+            return etalsProduitsRecherche;
+        }
+
+        private Etal trouverVendeur(Gaulois gaulois) {
+            boolean vendeurTrouve = false;
+            Etal etalVendeur = null;
+            for (int i = 0; i < etals.length && !vendeurTrouve; i++) {
+                Gaulois vendeur = etals[i].getVendeur();
+                if (vendeur != null) {
+                    vendeurTrouve = vendeur.getNom().equals(gaulois.getNom());
+                    if (vendeurTrouve) {
+                        etalVendeur = etals[i];
+                    }
+                }
+            }
+            return etalVendeur;
+        }
+        
+        private String afficherMarche() {
+            StringBuilder chaine = new StringBuilder();
+            int nbEtalLibre = 0;
+
+            for (Etal etal : etals) {
+                if (etal.isEtalOccupe()) {
+                    chaine.append(etal.afficherEtal());
+                } else {
+                    nbEtalLibre++;
+                }
+            }
+
+            chaine.append("Il reste " + nbEtalLibre + " étals non utilisés dans le marché.\n");
+            return chaine.toString();
+        }
+
+    } // Fin de la classe Marche
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setChef(Chef chef) {
+        this.chef = chef;
+    }
+
+    public void ajouterHabitant(Gaulois gaulois) {
+        if (nbVillageois < villageois.length) {
+            villageois[nbVillageois] = gaulois;
+            nbVillageois++;
+        }
+    }
+
+    public Gaulois trouverHabitant(String nomGaulois) {
+        if (nomGaulois.equals(chef.getNom())) {
+            return chef;
+        }
+        for (int i = 0; i < nbVillageois; i++) {
+            Gaulois gaulois = villageois[i];
+            if (gaulois.getNom().equals(nomGaulois)) {
+                return gaulois;
+            }
+        }
+        return null;
+    }
+
+    public String afficherVillageois() {
+        StringBuilder chaine = new StringBuilder();
+        if (nbVillageois < 1) {
+            chaine.append("Il n'y a encore aucun habitant au village du chef " + chef.getNom() + ".\n");
+        } else {
+            chaine.append("Au village du chef " + chef.getNom() + " vivent les légendaires gaulois :\n");
+            for (int i = 0; i < nbVillageois; i++) {
+                chaine.append("- " + villageois[i].getNom() + "\n");
+            }
+        }
+        return chaine.toString();
+    }
+} // Fin de la classe Village
